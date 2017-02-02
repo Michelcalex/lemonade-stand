7e7464088949ee9618fc7e88e37089a2ff773af7
Michelcalex <Michelcalex+github@gmail.com>
2017-02-02 17:53:32 -0500

    Get stand info from GET request

--- js/services/lemonaidservice.js
+++ js/services/lemonaidservice.js
@@ -3,6 +3,12 @@ module.exports = {
     func: function($http, $state) {
         const allLemonaidStands =[];
 
+        //TODO: ingredients, stats
+        const stats = [];
+
+        const ingredients = [];
+
+
         return {
             addStand(standName) {
                 $http.post('https://blooming-hamlet-70507.herokuapp.com/stand', {
@@ -15,17 +21,30 @@ module.exports = {
                     });
                     $state.go('manage-inventory');
 
-                // console.log('AFTER', allLemonaidStands);
+                    //console.log('AFTER', allLemonaidStands);
 
                 }).catch(function(error) {
                     // will run if error in POST
                 });
             },
             getStand(standId) {
+                $http.get('https://blooming-hamlet-70507.herokuapp.com/stand/' + standId)
+                    .then(function (info) {
+                    stats.push({
+                        day: info.data.day,
+                        balance: info.data.business.balance,
+                        vistors: info.data.business.yesterday_visitors,
+                        customers: info.data.business.yesterday_cups_sold,
+                    });
+                });
                 return $http.get('https://blooming-hamlet-70507.herokuapp.com/stand/' + standId);
             },
             getLemonaidStands() {
                 return allLemonaidStands;
+            },
+
+            getCurrentStand() {
+                return allLemonaidStands[allLemonaidStands.length - 1];
             },
         };
     }
