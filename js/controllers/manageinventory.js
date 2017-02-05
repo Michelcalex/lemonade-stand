4f31c1969897774f89651e50e395194b8453f4cf
Michelcalex <Michelcalex+github@gmail.com>
2017-02-05 00:20:14 -0500

    Cleaned up code for manage inventory controller. Updated inventory summary

--- js/controllers/manageinventory.js
+++ js/controllers/manageinventory.js
@@ -12,20 +12,43 @@ module.exports = {
                     let stand = response.data;
 
                     stand.name = currentStand.stand_name;
-                    stand.ingredients.forEach(function(ingredient) {
-                        if (ingredient.label === 'ice') {
+
+                    stand.ingredients.forEach(function(ingredient){
+                        if(ingredient.label === 'ice') {
                             ingredient.price = '$0.50';
                         }
-                        if (ingredient.label === 'lemons') {
-                            ingredient.price = '$2';
+                        if(ingredient.label === 'lemons') {
+                            ingredient.price = '$2.00';
                         }
-                        if (ingredient.label === 'sugar') {
+                        if(ingredient.label === 'ice') {
                             ingredient.price = '$1.25';
                         }
-                        if (ingredient.label === 'cups') {
+                        if(ingredient.label === 'ice') {
                             ingredient.price = '$0.10';
                         }
-                    });
+                    })
+
+
+
+                    // const listOfIngred = stand.ingredients;
+
+                    // for(let i = 0; i < listOfIngred.length; i++) {
+                    //     if(listOfIngred[i].label === 'ice') {
+                    //         listOfIngred[i].price = '$0.50';
+                    //     }
+                    //     if(listOfIngred[i].label === 'lemons') {
+                    //         listOfIngred[i].price = '$2.00';
+                    //     }
+                    //     if(listOfIngred[i].label === 'sugar') {
+                    //         listOfIngred[i].price = '$1.25';
+                    //     }
+                    //     if(listOfIngred[i].label === 'cups') {
+                    //         listOfIngred[i].price = '$0.10';
+                    //     }
+                    // }
+
+                    // $scope.listOfIngred = listOfIngred;
+            
 
                     $scope.stand = stand;
                     $scope.stats = [{
@@ -44,6 +67,10 @@ module.exports = {
                     
                     console.log($scope.stand);
                 });
+            
+
+
+
         }
     }
 }
